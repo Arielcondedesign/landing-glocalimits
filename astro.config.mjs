f2be97d97285ef2320689314570090856c37a937
Arielcondedesign <arielcondedesign@gmail.com>
2024-07-12 18:51:11 +0200

    Feat: add storie block

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,34 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
+import storyblok from "@storyblok/astro";
+import basicSsl from '@vitejs/plugin-basic-ssl'
+
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://master--kreatif-software.netlify.app",
-  integrations: [tailwind({ configFile: "./tailwind.config.mjs" })],
+
+  integrations: [
+    tailwind({ configFile: "./tailwind.config.mjs" }),
+    storyblok({
+      accessToken: "QECZX4W5lTkJfEyhKLXxJwtt",
+      bridge: {
+        customParent: 'https://app.storyblok.com',
+      },
+      components: {
+        page: 'storyblok/Page',
+        feature: 'storyblok/Feature',
+        grid: 'storyblok/Grid',
+        teaser: 'storyblok/Teaser',
+      },
+    }),
+  ],
+
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true,
+    },
+  },
 });
